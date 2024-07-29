3f292052eec283537ae8330f703b98e6f195e5bc
nico <cryptocoindev@protonmail.com>
2024-07-29 17:33:29 -0500

    mainnet

--- src/helpers/wagmiConfig.ts
+++ src/helpers/wagmiConfig.ts
@@ -4,7 +4,7 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 // Define KalyChain
 const kalyChain = {
-  id: 3889, // chain ID of KalyChain
+  id: 3888, // chain ID of KalyChain
   name: 'KalyChain',
   network: 'kalychain',
   nativeCurrency: {
@@ -13,8 +13,8 @@ const kalyChain = {
     symbol: 'KLC',
   },
   rpcUrls: {
-    public: { http: ['https://testnetrpc.kalychain.io/rpc'] }, // RPC URL
-    default: { http: ['https://testnetrpc.kalychain.io/rpc'] }, // RPC URL
+    public: { http: ['https://rpc.kalychain.io/rpc'] }, // RPC URL
+    default: { http: ['https://rpc.kalychain.io/rpc'] }, // RPC URL
   },
   blockExplorers: {
     default: { name: 'KalyScan', url: 'https://kalyscan.io' }, // Block explorer URL
